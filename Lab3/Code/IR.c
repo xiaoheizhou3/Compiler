f143d034e64d52c76328439029caada12e50c1c9
xiaoheizhou3 <zblackcater@163.com>
2016-05-20 08:56:50 -0700

    define some interface of intercode-list

--- Lab3/Code/IR.c
+++ Lab3/Code/IR.c
@@ -13,6 +13,177 @@ void insertCode(InterCodes* code){
     }
 }
 
-void deleteCode(InterCodes* code){
-    
+void deleteCode(InterCodes* code) {
+	if(code == code_head && code == code_tail){
+		code_head = NULL;
+		code_tail = NULL;
+	}
+	else if(code == code_head) {
+		if(code->next != NULL)
+			code->next->prev = NULL;
+		code_head = code->next;
+	}
+	else if(code == code_tail) {
+		if(code->prev != NULL)
+			code->prev->next = NULL;
+		code_tail = code->prev;
+	}
+	else {
+		if(code->next != NULL)
+			code->next->prev = code->prev;
+		if(code->prev != NULL)
+			code->prev->next = code->next;
+	}
+}
+
+void printCode(char* filename) {
+	FILE *fp=fopen(filename,"w");
+	if(fp == NULL) {
+		printf("open file error\n");
+		return;
+	}
+	InterCodes* index = code_head;
+	while(index != NULL)	{
+		switch(index->code->kind) {
+			case ASSIGN_K:
+				printOp(index->code->u.assign.left,fp);
+				fputs(":=	",fp);
+				printOp(index->code->u.assign.right,fp);
+				break;
+			case ADD_K:
+				printOp(index->code->u.binop.result,fp);
+				fputs(":=	",fp);
+				printOp(index->code->u.binop.op1,fp);
+				fputs("+	",fp);
+				printOp(index->code->u.binop.op2,fp);
+				break;
+			case SUB_K:
+				printOp(index->code->u.binop.result,fp);
+				fputs(":=	",fp);
+				printOp(index->code->u.binop.op1,fp);
+				fputs("-	",fp);
+				printOp(index->code->u.binop.op2,fp);
+				break;
+			case MUL_K:
+				printOp(index->code->u.binop.result,fp);
+				fputs(":=	",fp);
+				printOp(index->code->u.binop.op1,fp);
+				fputs("*	",fp);
+				printOp(index->code->u.binop.op2,fp);
+				break;
+			case DIV_K:
+				printOp(index->code->u.binop.result,fp);
+				fputs(":=	",fp);
+				printOp(index->code->u.binop.op1,fp);
+				fputs("/	",fp);
+				printOp(index->code->u.binop.op2,fp);
+				break;
+			case RETURN_K:
+				fputs("RETURN	",fp);
+				printOp(index->code->u.one.op,fp);
+				break;
+			case LABEL_K:
+				fputs("LABEL	",fp);
+				printOp(index->code->u.one.op,fp);
+				fputs(":	",fp);
+				break;
+			case GOTO_K:
+				fputs("GOTO	",fp);
+				printOp(index->code->u.one.op,fp);
+				break;
+			case IFGOTO_K:
+				fputs("IF	",fp);
+				printOp(index->code->u.triop.t1,fp);
+				fputs(index->code->u.triop.op,fp);
+				fputs("	",fp);
+				printOp(index->code->u.triop.t2,fp);
+				fputs("GOTO	",fp);
+				printOp(index->code->u.triop.label,fp);
+				break;
+			case READ_K:
+				fputs("READ	",fp);
+				printOp(index->code->u.one.op,fp);
+				break;
+			case WRITE_K:
+				fputs("WRITE	",fp);
+				printOp(index->code->u.one.op,fp);
+				break;
+			case CALL_K:
+				printOp(index->code->u.assign.left,fp);
+				fputs(":=	CALL	",fp);
+				printOp(index->code->u.assign.right,fp);
+				break;
+			case ARG_K:
+				fputs("ARG	",fp);
+				printOp(index->code->u.one.op,fp);
+				break;
+			case FUNCTION_K:
+				fputs("FUNCTION	",fp);
+				printOp(index->code->u.one.op,fp);
+				fputs(":",fp);
+				break;
+			case PARAM_K:
+				fputs("PARAM	",fp);
+				printOp(index->code->u.one.op,fp);
+				break;
+			case RIGHTAT_K:
+				printOp(index->code->u.assign.left,fp);
+				fputs(":=	&",fp);
+				printOp(index->code->u.assign.right,fp);
+				break;
+			case DEC_K:
+				fputs("DEC	",fp);
+				printOp(index->code->u.dec.op,fp);
+				char size[32];
+				sprintf(size,"%d",index->code->u.dec.size);
+				fputs(size,fp);
+				break;
+		}
+		fputs("\n",fp);
+		index = index->next;
+	}
+	fclose(fp);
+}
+
+void printOp(Operand* op,FILE* fp)
+{
+	if(op == NULL)
+	{
+		fputs("t0	",fp);
+		return;
+	}
+
+	char var[32];
+	memset(var,0,sizeof(var));
+	switch(op->kind){
+		case TEMPVAR:
+			fputs("t",fp);
+			sprintf(var,"%d",op->u.var_no);
+			fputs(var,fp);
+			break;
+		case VARIABLE:
+			fputs(op->u.value,fp);
+			break;
+		case CONSTANT:
+			fputs("#",fp);
+			fputs(op->u.value,fp);
+			break;
+		case LABEL:
+			fputs("label",fp);
+			sprintf(var,"%d",op->u.var_no);
+			fputs(var,fp);
+			break;
+		case FUNCTION:
+			fputs(op->u.value,fp);
+			break;
+		case TADDRESS: fputs("*t",fp);
+			sprintf(var,"%d",op->u.name->u.var_no);
+			fputs(var,fp);
+			break;
+		case VADDRESS:
+			fputs("*",fp);
+			fputs(op->u.name->u.value,fp);
+			break;
+	}
+	fputs("	",fp);
 }
--- Lab3/Code/IR.h
+++ Lab3/Code/IR.h
@@ -2,39 +2,86 @@
 #define _IR_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct Operand_* Operand;
+#define TEMPVAR  0
+#define VARIABLE 1
+#define CONSTANT 2
+#define VADDRESS  3
+#define LABEL    4
+#define FUNCTION 5
+#define TADDRESS 6
+#define ASSIGN_K 0
+#define ADD_K 1
+#define SUB_K 2
+#define MUL_K 3
+#define DIV_K 4
+#define RETURN_K 5
+#define LABEL_K  6
+#define GOTO_K   7
+#define IFGOTO_K 8
+#define READ_K	9
+#define WRITE_K 10
+#define CALL_K	11
+#define ARG_K	12
+#define FUNCTION_K 13
+#define PARAM_K	14
+#define DEC_K 15
+#define RIGHTAT_K  16
+
+typedef struct Operand_ Operand;
 typedef struct InterCode_ InterCode;
 typedef struct InterCodes_ InterCodes;
 struct Operand_ {
-    enum {varible,contsant,address,/*to add*/} kind;
+    enum {tempvar,variable,cons,vaddress,label,function,taddress} kind;
     union {
         int var_no;
-        int value;
-        /*to add*/
-    } u;
+        char* value;
+        Operand* name;
+    }u;
+    Operand* next;
 };
 
 struct InterCode_ {
-    enum {assign,add,sub,mul,/*to add*/} kind;
+    enum {assign_k,add_k,sub_k,mul_k,div_k,return_k,label_k,goto_k,ifgoto_k,read_k,write_k,call_k,arg_k,function_k,param_k,dec_k,rightat_k} kind;
     union {
-        struct { Operand right,left;} assign;
-        struct { Operand result,op1,op2;} binop;
-        /*to add */
-    };
+        struct{			//return ,label,goto,read,write,arg, function
+			Operand* op;		//param
+		} one;
+		struct{
+			Operand *left,*right;	//call
+		} assign;	//assign	rightat
+
+		struct{
+			Operand *result,*op1,*op2;
+		} binop;			//add sub mul div
+
+		struct{
+			Operand *t1;
+			char *op;
+			Operand *t2,*label;
+		} triop;			//if_goto
+
+		struct{
+			Operand *op;
+			int size;
+		} dec; //dec
+    }u;
 };
 
 struct InterCodes_ {
-    InterCode code;
+    InterCode* code;
     InterCodes* prev;
     InterCodes* next;
 };
+
 
 InterCodes *code_head = NULL,*code_tail = NULL;
 
 void insertCode(InterCodes* code);
 void deleteCode(InterCodes* code);
-
+void printCode(char* filename);
+void printOp(Operand* op,FILE* fp);
 
 
 #endif
